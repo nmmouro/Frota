52913b4448c9aeb68385b4b56e7d5dc14b19cb20
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-16 16:17:09 -0300

    Update API_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
   API_URL:
-    "https://script.google.com/macros/s/AKfycbzVGlFgPiSMDWXo3vf3aaYtFtEjd2dSMM4hcvFvNb8ALNiuEH70gkMEa-tzj3y_Q_tKkw/exec"
+    "https://script.google.com/macros/s/AKfycbwhTKSkVD9GEJc-2irlmzomBkKd8nVrzM4h18WdiZ3fNdfSoUADtO9VwmJ4_BtXAukNHA/exec"
 };
