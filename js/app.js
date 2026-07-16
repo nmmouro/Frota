fd4a544dff1067d217014454bbbe43b3b341e868
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-16 17:45:23 -0300

    Import and use obterLancamentos and renderTabela

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+import {
+    obterLancamentos
+}
+from "./services/lancamentos.js";
 
+import {
+    renderTabela
+}
+from "./ui/table.js";
+
+const dados =
+    await obterLancamentos();
+
+renderTabela(
+
+    document.getElementById("tabela"),
+
+    dados
+
+);
