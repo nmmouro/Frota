ea583a48a6487869f949b2ae42b078d84949f688
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-17 13:09:44 -0300

    Refactor app.js to use async function for data rendering
    Refactor imports and encapsulate logic in 'iniciar' function.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
-import {
-    obterLancamentos
+import { obterLancamentos }
+    from "./services/lancamentos.js";
+
+import { renderTabela }
+    from "./ui/table.js";
+
+async function iniciar() {
+
+    try {
+
+        const dados = await obterLancamentos();
+
+        renderTabela(
+            document.getElementById("tabela"),
+            dados
+        );
+
+    } catch (erro) {
+
+        console.error(erro);
+
+    }
+
 }
-from "../services/lancamentos.js";
 
-import {
-    renderTabela
-}
-from "../ui/table.js";
-
-const dados =
-    await obterLancamentos();
-
-renderTabela(
-
-    document.getElementById("tabela"),
-
-    dados
-
-);
+iniciar();
