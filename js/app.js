b814bd1e306a5b07460a38746bd9d05dc26a52f1
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-17 20:15:59 -0300

    Implement data loading and rendering on DOMContentLoaded
    Add functionality to load and render data in a table.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,51 @@
+import {
+    obterLancamentos
+} from "./services/lancamentos.js";
+
+import {
+    renderTabela
+} from "./ui/table.js";
+
+document.addEventListener(
+    "DOMContentLoaded",
+    carregarTabela
+);
+
+async function carregarTabela() {
+
+    try {
+
+        const dados =
+            await obterLancamentos();
+
+        renderTabela(
+            document.getElementById("tabela"),
+            dados
+        );
+
+    }
+    catch (erro) {
+
+        console.error(erro);
+
+        document.getElementById("tabela").innerHTML = `
+            <p class="erro">
+                Erro ao carregar os lançamentos.
+            </p>
+        `;
+
+    }
+
+}
+
+
+/*
+
+
+
+
+
+
 import { obterLancamentos }
     from "../services/lancamentos.js";
 
@@ -24,3 +72,6 @@ async function iniciar() {
 }
 
 iniciar();
+
+
+*/
