ac9463d53ebf5a34b23d530008e20d076096c8f9
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-17 13:17:00 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 import { obterLancamentos }
-    from "./services/lancamentos.js";
+    from "../services/lancamentos.js";
 
 import { renderTabela }
-    from "./ui/table.js";
+    from "../ui/table.js";
 
 async function iniciar() {
 
