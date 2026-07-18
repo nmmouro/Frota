312da11ac64cff11833c6f6326543cb4387bafb9
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-17 21:06:30 -0300

    Create config.js

---
+++ js/config/config.js
@@ -0,0 +1,19 @@
+export const CONFIG = {
+
+    API:
+
+    "https://script.google.com/macros/s/AKfycbxDxS7bONLdSSnEA9SSq87jBm4uzyGntV6aqegxgI_8tyhdX3ep5Wd-TYErVNpCdD25/exec",
+
+    REFRESH:
+
+    15000,
+
+    STATUS:{
+
+        AGENDADO:"#3498db",
+
+        CONCLUIDO:"#2ecc71"
+
+    }
+
+}
