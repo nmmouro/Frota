56488bd65b4a57446db595a638f2530478291253
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-17 21:33:16 -0300

    Update lancamentos.js

--- js/services/lancamentos.js
+++ js/services/lancamentos.js
@@ -1,3 +1,55 @@
+// ================= IMPORTS =================
+
+import {
+    apiGet,
+    apiPost,
+    apiPut,
+    apiDelete
+} from "../app/api.js";
+
+// ================= ENDPOINT =================
+
+const ENDPOINT = "lancamentos";
+
+// ================= CONSULTAS =================
+
+export async function obterLancamentos() {
+
+    return await apiGet(ENDPOINT);
+
+}
+
+export async function obterLancamento(id) {
+
+    return await apiGet(`${ENDPOINT}/${id}`);
+
+}
+
+// ================= GRAVAÇÃO =================
+
+export async function salvarLancamento(dados) {
+
+    return await apiPost(ENDPOINT, dados);
+
+}
+
+export async function atualizarLancamento(id, dados) {
+
+    return await apiPut(`${ENDPOINT}/${id}`, dados);
+
+}
+
+// ================= EXCLUSÃO =================
+
+export async function excluirLancamento(id) {
+
+    return await apiDelete(`${ENDPOINT}/${id}`);
+
+}
+
+
+
+/*
 import { listar } from "../api/api.js";
 
 export async function obterLancamentos() {
@@ -11,3 +63,4 @@ export async function excluirLancamento(){...}
 export async function atualizarLancamento(){...}
 
 export async function obterLancamento(id){...}
+*/
