fed594bd95f59a4613c3ca58e85fa54221959e64
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-18 09:27:16 -0300

    Refactor API config and update URL
    Refactor API configuration structure and update URL.

--- js/config/config.js
+++ js/config/config.js
@@ -1,19 +1,13 @@
-export const CONFIG = {
+// ================= API =================
 
-    API:
+export const API = {
 
-    "https://script.google.com/macros/s/AKfycbxDxS7bONLdSSnEA9SSq87jBm4uzyGntV6aqegxgI_8tyhdX3ep5Wd-TYErVNpCdD25/exec",
+    URL: "https://script.google.com/macros/s/AKfycbxDxS7bONLdSSnEA9SSq87jBm4uzyGntV6aqegxgI_8tyhdX3ep5Wd-TYErVNpCdD25/exec",
 
-    REFRESH:
+    HEADERS: {
 
-    15000,
-
-    STATUS:{
-
-        AGENDADO:"#3498db",
-
-        CONCLUIDO:"#2ecc71"
+        "Content-Type": "application/json"
 
     }
 
-}
+};
