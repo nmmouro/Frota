ddc2885784123b75d5396c9b27aff2a81c78688d
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-20 21:27:30 -0300

    Update lancamentos.js

--- js/services/lancamentos.js
+++ js/services/lancamentos.js
@@ -39,18 +39,18 @@ export async function obterLancamentos() {
     return listar(ABA);
 }
 
-export async function obterLancamentos(id) {
+export async function obterLancamento(id) {
     return buscar(ABA, id);
 }
 
-export async function salvarLancamentos(dados) {
+export async function salvarLancamento(dados) {
     return salvar(ABA, dados);
 }
 
-export async function atualizarLancamentos(id, dados) {
+export async function atualizarLancamento(id, dados) {
     return editar(ABA, id, dados);
 }
 
-export async function excluirLancamentos(id) {
+export async function excluirLancamento(id) {
     return excluir(ABA, id);
 }
