f1e84817c9415da23678bef4496b37adaf7b6514
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-26 15:49:38 -0300

    Update api.js

--- js/api/api.js
+++ js/api/api.js
@@ -26,6 +26,10 @@ import {
 async function request(url, options = {}) {
 
     console.log(
+        "🔥 API.JS NOVA VERSÃO EXECUTADA"
+    );
+
+    console.log(
         "API Request:",
         url
     );
