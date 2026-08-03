76e8c26d79478bdbda2aa428c9b71658081528e9
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-02 22:28:31 -0300

    Change API_URL to new Google Script endpoint
    Updated API_URL to a new endpoint.

--- js/config/config.js
+++ js/config/config.js
@@ -15,7 +15,7 @@ export const CONFIG = {
 
     API_URL:
 
-        "https://script.google.com/macros/s/AKfycbzwyrOh5l5SpSuVNDkzBqbmruSoA14hYFPb0DsG2vhTYAqeb2VpFZPOsnKwrbgXxfyNcQ/exec",
+        "https://script.google.com/macros/s/AKfycbxwAU3Vt57KynjDnWTPSOvi5oAEVMy82SAeKy2svSkledng4JGSKLXSbk_gNAOtIVTUIg/exec",
 
     UPDATE_INTERVAL: 30000,
 
